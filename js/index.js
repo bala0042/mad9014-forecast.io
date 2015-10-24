16f4c640fdf26639fe64e251915e04841e3a6f75
Noyan Balay <bala0042@algonquinlive.com>
2015-10-24 15:18:53 -0400

    bugfix

--- js/index.js
+++ js/index.js
@@ -51,7 +51,7 @@ function displayWidget(current,hourly,container) {
             var hourNode = $("<tr>");
             $("<td>").text(time).appendTo(hourNode);
             $("<td>").text("Hum. " + hourlyData.humidity.toString().split(".")[1] + "%").appendTo(hourNode);
-            $("<td>").text("Clo. " + (hourlyData.cloudCover == 1 ? "100%" : parseInt(hourlyData.cloudCover.toString().split(".")[1],10)) + "%").appendTo(hourNode);
+            $("<td>").text("Clo. " + (hourlyData.cloudCover == 1 ? "100%" : (parseInt(hourlyData.cloudCover.toString().split(".")[1],10) + "%"))).appendTo(hourNode);
             $("<td>").text(hourlyData.temperature + " \xB0C").appendTo(hourNode);
             $("<td>").text(hourlyData.windSpeed + " km/h").appendTo(hourNode);
             $("<td>").html($("<i>").addClass("wi").addClass("wi-forecast-io-" + hourlyData.icon)).appendTo(hourNode);
